2a0eb886363bb62403f4db40dbafadc52c38c809
ali <engalidanish@gmail.com>
2024-10-04 02:47:20 +0200

    remove unused code

--- index.ts
+++ index.ts
@@ -3,17 +3,9 @@ import { Pipe } from '@baseai/core';
 import inquirer from 'inquirer';
 import ora from 'ora';
 import chalk from 'chalk';
-import TerminalRenderer from 'marked-terminal';
 import pipeProofreaderAgent from './baseai/pipes/proofreader-agent';
-import { marked } from 'marked';
 
 const pipe = new Pipe(pipeProofreaderAgent());
-
-// Set up marked with the terminal renderer
-// marked.setOptions({
-//     renderer: new TerminalRenderer()
-//   });
-  
 
 async function main() {
     console.log(chalk.green('Hi I am Proofreader, you can now proofread your docs that are placed in memory!'));
@@ -43,7 +35,6 @@ async function main() {
             spinner.stop();
             console.log(chalk.cyan('Agent:'));
             console.log(linuxAgentResp);
-            // console.log(marked(linuxAgentResp));
         } catch (error) {
             spinner.stop();
             console.error(chalk.red('Error processing your request:'), error);
