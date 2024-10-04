b327ca73b900b9802090f9a5a5dcc53ea00bbb46
ali <engalidanish@gmail.com>
2024-10-04 12:50:58 +0200

    updated user console info

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import pipeProofreaderAgent from './baseai/pipes/proofreader-agent';
 const pipe = new Pipe(pipeProofreaderAgent());
 
 async function main() {
-    console.log(chalk.green('Hi I am Proofreader, you can now proofread your docs that are placed in memory!'));
+    console.log(chalk.green('Hi I am Proofreader, enter \"proofread the attach document from CONTEXT\"!'));
     console.log(chalk.yellow('Type "exit" to quit the application.\n'));
 
     while (true) {
@@ -28,13 +28,13 @@ async function main() {
         const spinner = ora('Processing your request...').start();
 
         try {
-            const { completion: linuxAgentResp } = await pipe.run({
+            const { completion: proofReaderResponse } = await pipe.run({
                 messages: [{ role: 'user', content: userMsg }],
             });
 
             spinner.stop();
             console.log(chalk.cyan('Agent:'));
-            console.log(linuxAgentResp);
+            console.log(proofReaderResponse);
         } catch (error) {
             spinner.stop();
             console.error(chalk.red('Error processing your request:'), error);
